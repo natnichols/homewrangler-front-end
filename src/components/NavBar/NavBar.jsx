c3226574aa0efa5243c8966d1fc7669967ee294f
rbozek <rbozek@gmail.com>
2024-02-10 13:30:02 -0500

    add safety message, change lis to divs

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,13 +6,16 @@ const NavBar = ({ user, handleLogout }) => {
     <nav>
       {user ?
         <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+          <div>Welcome, {user.name}</div>
+          <div><NavLink to="/profiles">Profiles</NavLink></div>
+          <div><NavLink to="/auth/change-password">Change Password</NavLink></div>
+          <div><NavLink to="/auth/login">Log In</NavLink></div>
+          <div><NavLink to="/auth/signup">Sign Up</NavLink></div>
+          <div><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></div>
         </ul>
       :
         <ul>
+          <li>how are you seeing this? leaving links for safety til later</li>
           <li><NavLink to="/auth/login">Log In</NavLink></li>
           <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
         </ul>
