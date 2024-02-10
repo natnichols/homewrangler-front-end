7e523436a35f7df446913341cc3c0a739795733a
rbozek <rbozek@gmail.com>
2024-02-10 13:46:00 -0500

    stub up correct link text (but not links yet), add Link for only single Profile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,16 +8,20 @@ const NavBar = ({ user, handleLogout }) => {
     <nav>
       {user ?
         <div className={styles.container}>
-          <div>Welcome, {user.name}</div>
-          <div><NavLink to="/profiles">Profiles</NavLink></div>
-          <div><NavLink to="/auth/change-password">Change Password</NavLink></div>
-          <div><NavLink to="/auth/login">Log In</NavLink></div>
-          <div><NavLink to="/auth/signup">Sign Up</NavLink></div>
+          <div><NavLink to="/profiles/:profileId">User Profile</NavLink></div>
+          <div>Shipping List</div>
+          <div>My Pantry</div>
+          <div>Repairs</div>
+          <div>Budget</div>
           <div><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></div>
         </div>
       :
         <ul>
-          <li>how are you seeing this? leaving links for safety til later</li>
+          <li>leaving for safety - will be removed eventually</li>
+          <li>Welcome, {user.name}</li>
+          <li><NavLink to="/profiles">Profiles</NavLink></li>
+          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
           <li><NavLink to="/auth/login">Log In</NavLink></li>
           <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
         </ul>
