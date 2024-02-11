02d798930f90d888c264beb2bbf66aca0b8e6cd2
rbozek <rbozek@gmail.com>
2024-02-11 12:17:09 -0500

    finish all basic placement for landing ternary items

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,25 +7,38 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>Landing component</h1>
-      <h2>howdy, {user ? user.name : 'pardner'}</h2>
-      <h2>test section below:</h2>
+      {/* <h1>Landing component</h1> */}
+      {/* <h2>test section below:</h2> */}
       {user ? (
-      <>
+        <>
         'LOGGED-IN USER SHOULD SEE THIS'
         <h1>HomeWrangler</h1>
+        <h2>howdy, {user ? user.name : 'pardner'}</h2>
+        <br></br>
+        <div>Shopping List button</div>
+        <div><NavLink to="/pantryItems">My Pantry button</NavLink></div>
+        <br></br>
+        <div>Repairs button</div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <div>Budget button</div>
+
 
       </>
       ) : (
-      <>
-        'NOT-LOGGED IN SHOULD SEE THIS'
+        <>
+        NOT-LOGGED IN SHOULD SEE THIS:
+        <h1>HomeWrangler</h1>
+        <h4>(logo will go here)</h4>
+        <h3>"catchy app slogan goes here"</h3>
+
+
         <div><NavLink to="/auth/login">Log In button</NavLink></div>
         <div><NavLink to="/auth/signup">Sign Up button</NavLink></div>
       </>
       )}
 
 
-      <h4>end of Landing component</h4>
+      <h4>(end of Landing component)</h4>
     </main>
   )
 }
