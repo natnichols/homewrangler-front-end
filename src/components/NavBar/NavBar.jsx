9cf7182295c3f1f7eea533987a57db62097ac256
rbozek <rbozek@gmail.com>
2024-02-11 12:48:15 -0500

    change structure from ternary to single if statement

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,34 +4,24 @@ import { NavLink } from 'react-router-dom'
 import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
-  return (
-    <nav>
-      {user ?
-        <div className={styles.container}>
-          <div><NavLink to="/profiles/:profileId">User Profile</NavLink></div>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <div>Shopping List /</div>
-          <div><NavLink to="/pantryItems">My Pantry</NavLink></div>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <div>Repairs</div>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <div>Budget</div>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <div><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></div>
-        </div>
-      :
-        <ul>
-          <li>leaving for safety - will be removed eventually</li>
-          {/* <li>Welcome, {user.name}</li> */}
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
-      }
-    </nav>
-  )
+  if (user) {
+    return (
+      <nav>
+          <div className={styles.container}>
+            <div><NavLink to="/profiles/:profileId">🤠</NavLink></div>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div>🛒</div> / 
+            <div><NavLink to="/pantryItems">🥫</NavLink></div>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div>🔧</div>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div>💰</div>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <div><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></div>
+          </div>
+      </nav>
+    )
+  }
 }
 
 export default NavBar
