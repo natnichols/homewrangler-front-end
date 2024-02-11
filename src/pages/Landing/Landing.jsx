d4fff7e3b9bc29c9a059466215e3b684edd2b071
rbozek <rbozek@gmail.com>
2024-02-11 12:08:10 -0500

    prepare large ternary for landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,8 +5,22 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>Landing component</h1>
-      <h2>hello, {user ? user.name : 'friend'}</h2>
-      <h3>end of Landing component</h3>
+      <h2>howdy, {user ? user.name : 'pardner'}</h2>
+      <h2>test section below:</h2>
+      {user ? (
+      <>
+        'LOGGED-IN USER SHOULD SEE THIS'
+        <h1>HomeWrangler</h1>
+
+      </>
+      ) : (
+      <>
+        'NOT-LOGGED IN SHOULD SEE THIS'
+      </>
+      )}
+
+
+      <h4>end of Landing component</h4>
     </main>
   )
 }
