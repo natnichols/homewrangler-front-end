a30bfa4b079980252f9c024f9a855ba98db865e0
rbozek <rbozek@gmail.com>
2024-02-11 16:15:19 -0500

    stub up buttonz

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,5 @@
-// // npm modules
+// npm modules
+
 // import { useState, useEffect } from 'react'
 
 // // services
@@ -10,21 +11,23 @@ import styles from './Profile.module.css'
 const Profile = ({ user }) => {
   {console.log('test display username1', user.name)}  
   
-  const profilePhoto = user.profile.photo ? user.profile.photo : ''
+  // const profilePhoto = user.profile.photo ? user.profile.photo : ''
 
   return (
     <main className={styles.container}>
       <h1>Single profile component.</h1>
 
-        <p><img src={profilePhoto} alt="The user's avatar" />
-        </p>
+        {/* <p>
+          <img src={user.profile.photo} alt="The user's avatar" />
+        </p> */}
+
         <p>{user.name}</p>
         <p>{user.email}</p>
-        <p>change password button</p>
-        <p>edit profile button (stretch)</p>
+        <button>change password</button>
+        <button>edit profile (not active - stretch)</button>
 
 
-      <h3>end of Profile component</h3>
+      <h4>end of Profile component</h4>
     </main>
   )
 }
