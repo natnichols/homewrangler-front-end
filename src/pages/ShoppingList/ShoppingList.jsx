c14db863d3a9f4acaf339b6ad3517c0ce9efed1b
rbozek <rbozek@gmail.com>
2024-02-12 18:59:28 -0500

    import & add to parent budget component, & import-add BudgetAvail to ShoppingList page component

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -1,10 +1,15 @@
 //css
 import styles from './ShoppingList.module.css'
 
+//components
+import BudgetAvail from '../../components/BudgetAvail/BudgetAvail';
+
 const ShoppingList = () => {
   return (
     <main className={styles.container}>
       <h1>ShoppingList component</h1>
+
+        <BudgetAvail/>
 
       <h4>end of ShoppingList component</h4>
     </main>
