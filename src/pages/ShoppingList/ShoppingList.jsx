c6eba09f49dc24b3f3d141da0a0ee98789907241
rbozek <rbozek@gmail.com>
2024-02-12 16:53:37 -0500

    create directories & files for Repair & Budget components, stub up.

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -3,11 +3,11 @@ import styles from './ShoppingList.module.css'
 
 const ShoppingList = () => {
   return (
-    <>
+    <main className={styles.container}>
       <h1>ShoppingList component</h1>
 
       <h4>end of ShoppingList component</h4>
-    </>
+    </main>
   )
 }
 
