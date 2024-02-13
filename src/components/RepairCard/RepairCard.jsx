b85d9a31aaec055b60832238bd946a744a457ac3
rbozek <rbozek@gmail.com>
2024-02-12 19:06:13 -0500

    create RepairAdd & RepairCard components

---
+++ src/components/RepairCard/RepairCard.jsx
@@ -0,0 +1,14 @@
+//css
+import styles from './RepairCard.module.css'
+
+const RepairCard = () => {
+  return (
+    <main className={styles.container}>
+      <h3>RepairCard component</h3>
+
+      <h4>end of RepairCard component</h4>
+    </main>
+  )
+}
+
+export default RepairCard;
