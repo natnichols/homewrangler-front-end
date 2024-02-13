850ae3152738991f50d65c521ed4f9115071b003
NatNichols <natalie.e.nichols@gmail.com>
2024-02-13 15:57:24 -0500

    add basic styling to RepairAdd component, set formData state, write handleSubmit and handleChange functions, remove placeholder headers from return and begin building form

--- src/components/RepairAdd/RepairAdd.jsx
+++ src/components/RepairAdd/RepairAdd.jsx
@@ -1,12 +1,30 @@
-//css
+// npm modules
+import { useState } from 'react'
+
+// css
 import styles from './RepairAdd.module.css'
 
-const RepairAdd = () => {
+const RepairAdd = (props) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    priority: '',
+    description: '',
+  })
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    props.handleAddRepair(formData)
+  }
+
+  const handleChange = evt => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
   return (
     <main className={styles.container}>
-      <h3>RepairAdd component</h3>
-
-      <h4>end of RepairAdd component</h4>
+      <form onSubmit={handleSubmit}>
+        
+      </form>
     </main>
   )
 }
