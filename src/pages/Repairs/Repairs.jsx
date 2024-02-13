0f703425a08fa85ce8e7916972372acc8954bdca
rbozek <rbozek@gmail.com>
2024-02-12 19:21:02 -0500

    cleanup touches

--- src/pages/Repairs/Repairs.jsx
+++ src/pages/Repairs/Repairs.jsx
@@ -1,15 +1,22 @@
 //css
 import styles from './Repairs.module.css'
 
+//component
+import BudgetAvail from '../../components/BudgetAvail/BudgetAvail';
+import RepairCard from '../../components/RepairCard/RepairCard';
+
 const Repairs = () => {
   return (
     <main className={styles.container}>
       <h1>Repairs component</h1>
 
-        <p>Link to Add Repair will go here (or whole component)</p>
-        <p>Link to Edit Repair will go here</p>
-        <p>Link to Repair Details will go here</p>
+        <p>Link to Add Repair here (or whole component)</p>
+        <p>Link to Edit Repair here</p>
+        <p>Link to Repair Details here</p>
 
+
+        <BudgetAvail />
+        <RepairCard />
       <h4>end of Repairs component</h4>
     </main>
   )
