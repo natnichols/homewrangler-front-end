0aa589ba958e97fae868635b0090cdd4621c6f68
rbozek <rbozek@gmail.com>
2024-02-12 20:28:52 -0500

    update all buttons with proper links, clean up test text top of page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -10,19 +10,18 @@ const Landing = ({ user }) => {
       {user ? (
       <>
         <br></br>
-        'LOGGED-IN USER SHOULD SEE THIS'
-        <h1>HomeWrangler main landing</h1>
+        <h1>HomeWrangler</h1>
         <h2>howdy, {user ? user.name : 'pardner'}</h2>
         <br></br>
-        <button>Shopping List (not active)</button>
+        <NavLink to={`/pantryItems/${user.profile}`}><button>Shopping List</button></NavLink>
         <br></br>
         <NavLink to="/pantryItems"><button>My Pantry</button></NavLink>
         <br></br>
         <br></br>
-        <button>Repairs (not active)</button>
+        <NavLink to="/repairs"><button>Repairs</button></NavLink>
         <br></br>
         <br></br>
-        <button>Budget button (not active)</button>
+        <NavLink to="/budgets"><button>Budget button</button></NavLink>
       </>
       ) : (
       <>
