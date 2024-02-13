42ec8a9744f94f1afa923191cf4e126f43f39fd3
rbozek <rbozek@gmail.com>
2024-02-12 19:15:43 -0500

    some more edit to Repairs component

--- src/pages/Repairs/Repairs.jsx
+++ src/pages/Repairs/Repairs.jsx
@@ -6,6 +6,10 @@ const Repairs = () => {
     <main className={styles.container}>
       <h1>Repairs component</h1>
 
+        <p>Link to Add Repair will go here (or whole component)</p>
+        <p>Link to Edit Repair will go here</p>
+        <p>Link to Repair Details will go here</p>
+
       <h4>end of Repairs component</h4>
     </main>
   )
