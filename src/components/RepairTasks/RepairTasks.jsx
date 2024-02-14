c67e5d74ccad0cbf7f33496b3efcf4c06fa9831b
NatNichols <natalie.e.nichols@gmail.com>
2024-02-13 21:19:06 -0500

    stub up RepairTasks component

--- src/components/RepairTasks/RepairTasks.jsx
+++ src/components/RepairTasks/RepairTasks.jsx
@@ -4,3 +4,10 @@ import RepairTaskCard from '../RepairTaskCard/RepairTaskCard'
 // css
 import styles from './RepairTasks.module.css'
 
+const RepairTasks = (props) => {
+  return ( 
+    
+  )
+}
+
+export default RepairTasks
