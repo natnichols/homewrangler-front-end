88fc0d1b0b21eec585f49c169644809fb0e21427
NatNichols <natalie.e.nichols@gmail.com>
2024-02-13 21:22:28 -0500

    map through repairTasks in RepairTasks component

--- src/components/RepairTasks/RepairTasks.jsx
+++ src/components/RepairTasks/RepairTasks.jsx
@@ -5,8 +5,18 @@ import RepairTaskCard from '../RepairTaskCard/RepairTaskCard'
 import styles from './RepairTasks.module.css'
 
 const RepairTasks = (props) => {
+  if (!props.repairTasks.length) return <h4>No Repair Tasks</h4>
+
   return ( 
-    
+    <>
+      {props.repairTasks.map(repairTask => (
+        <RepairTaskCard 
+          key={repairTask._id}
+          repairTask={repairTask}
+          user={props.user}
+        />
+      ))}
+    </>
   )
 }
 
