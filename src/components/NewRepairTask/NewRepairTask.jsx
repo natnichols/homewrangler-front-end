4d8a90c4f0543b1c8f9faa6125cce32ac9f91b1b
NatNichols <natalie.e.nichols@gmail.com>
2024-02-13 20:42:36 -0500

    set formData in state in NewRepairTask component, write handleChange and handleSubmit functions for NewRepairTask

--- src/components/NewRepairTask/NewRepairTask.jsx
+++ src/components/NewRepairTask/NewRepairTask.jsx
@@ -7,6 +7,18 @@ import styles from './NewRepairTask.module.css'
 // components
 
 const NewRepairTask = (props) => {
+  const [formData, setFormData] = useState({ task: '' })
+
+  const handleChange = evt => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    // placeholder line
+    setFormData({ task: '' })
+  }
+
   return ( 
 
   )
