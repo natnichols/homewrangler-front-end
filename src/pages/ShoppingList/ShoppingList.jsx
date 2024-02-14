4d50b108cdd79c2d3e14b61bd81a3b08d61d56ed
rbozek <rbozek@gmail.com>
2024-02-14 15:23:42 -0500

    shoppingList.jsx - import usestate, useeffect, profile service, add same function from profile.jsx, edit to use props. instead of user.

--- src/pages/ShoppingList/ShoppingList.jsx
+++ src/pages/ShoppingList/ShoppingList.jsx
@@ -1,3 +1,9 @@
+//npm 
+import { useState, useEffect } from 'react';
+
+// services
+import * as profileService from '../../services/profileService'
+
 //css
 import styles from './ShoppingList.module.css'
 
@@ -6,11 +12,22 @@ import styles from './ShoppingList.module.css'
 import PantryCard from '../../components/PantryCard/PantryCard';
 
 const ShoppingList = (props) => {
-  console.log('test props.user ', props.user);
-  console.log('test props.user.profile ', props.user.profile);
-  console.log('test props.user.profile._id ', props.user.profile._id);
-  console.log('test props.user.profile.shoppingList ', props.user.profile.shoppingList);
+  // console.log('test props.user ', props.user);
+  // console.log('test props.user.profile ', props.user.profile);
+  // console.log('test props.user.profile._id ', props.user.profile._id);
+  // console.log('test props.user.profile.shoppingList ', props.user.profile.shoppingList);
 
+  const [profile, setProfile] = useState(null)
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const profileData = await profileService.getOneProfile(props.user.profile)
+      setProfile(profileData)
+    }
+    fetchProfile()
+  }, [props.user.profile])
+
+  console.log('test single profile: ', profile);
 
   return (
     <main className={styles.container}>
