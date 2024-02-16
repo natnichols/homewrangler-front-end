0847deb59d022d83139c9a452d785b93b54bfdbe
rbozek <rbozek@gmail.com>
2024-02-16 00:58:30 -0500

    minor adjust to Shopping Cards

--- src/components/ShoppingListCard/ShoppingListCard.jsx
+++ src/components/ShoppingListCard/ShoppingListCard.jsx
@@ -9,12 +9,13 @@ const ShoppingListCard = ({pantryItem,  handleDelFromShoppingList}) => {
     <main className={styles.container}>
       <div>
         <p>
+          🛒
+          <Link href="#" title="remove from shopping list" onClick={() => handleDelFromShoppingList(pantryItem._id)}>➖</Link>
+          &nbsp;&nbsp;
           <Link to={`/pantryItems/${pantryItem._id}`}>
             {pantryItem.name}
           </Link>
           &nbsp;&nbsp;
-          🛒&nbsp;
-          <Link href="#" title="remove from shopping list" onClick={() => handleDelFromShoppingList(pantryItem._id)}>➖</Link>
           &nbsp;&nbsp;
           { pantryItem.category ? (pantryItem.category + '\u00A0\u00A0') : ''}
           { pantryItem.price ? ('$' + pantryItem.price + '\u00A0\u00A0') : ''}
