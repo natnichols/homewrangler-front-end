5fb75da018164bd34a1b04f5916c741dbcade2b8
NatNichols <natalie.e.nichols@gmail.com>
2024-02-16 16:29:44 -0500

    fix indentation

--- src/components/RepairTasks/RepairTasks.jsx
+++ src/components/RepairTasks/RepairTasks.jsx
@@ -14,7 +14,7 @@ const RepairTasks = (props) => {
           repairTask={repairTask}
           handleDeleteRepairTask={props.handleDeleteRepairTask}
           />
-          ))}
+        ))}
     </>
   )
 }
